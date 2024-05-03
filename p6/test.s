93327ee3b6b7c80fbcbf9217178dd9f59ad4f71a
Leah Gunn <lgunn@royal-14.cs.wisc.edu>
2024-05-03 00:49:34 -0500

    Good start

---
+++ p6/test.s
@@ -0,0 +1,21 @@
+	.data
+	.align2
+_a:	.space
+	.data
+	.align2
+_b:	.space
+	.text
+	.globl main
+main:		# METHOD ENTRY
+	sw    $ra, 0($sp)	# PUSH
+	subu  $sp, $sp, 4
+	sw    $fp, 0($sp)	# PUSH
+	subu  $sp, $sp, 4
+	addu  $fp, $sp, 8
+	.text
+_<new_function>:		# METHOD ENTRY
+	sw    $ra, 0($sp)	# PUSH
+	subu  $sp, $sp, 4
+	sw    $fp, 0($sp)	# PUSH
+	subu  $sp, $sp, 4
+	addu  $fp, $sp, 8
